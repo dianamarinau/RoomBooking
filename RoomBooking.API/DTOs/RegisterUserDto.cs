c0930fc979e8479b8510821384019f3eec0c678d
Diana Marinau <marinaudiana22@gmail.com>
2026-04-29 14:37:13 +0300

    Add CreatedAt for users and remove Jwt key

---
+++ RoomBooking.API/DTOs/RegisterUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RoomBooking.API.DTOs
+{
+    public class RegisterUserDto
+    {
+        public int UserId { get; set; }
+        public string Fullname { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
--- RoomBooking.API/Models/User.cs
+++ RoomBooking.API/Models/User.cs
@@ -13,7 +13,7 @@ public partial class User
 
     public string PasswordHash { get; set; } = null!;
 
-    public DateTime? CreatedAt { get; set; }
+    public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
 
     public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
 
